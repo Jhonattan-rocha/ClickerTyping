07749e421ada59a7e2ecfd2c5fe8a59b182522b2
Jhonattan Rocha da Silva <97904111+Jhonattan-rocha@users.noreply.github.com>
2023-11-05 19:05:29 -0300

    patch

---
+++ Clicker/build-Clicker-Desktop_Qt_6_6_0_MinGW_64_bit-Release/Clicker_autogen/EWIEGA46WW/moc_mainwindow.cpp
@@ -0,0 +1,337 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../Clicker/mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.6.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "recordClicks",
+    "",
+    "recoverClicks",
+    "fs::path",
+    "path",
+    "showMsgRecord",
+    "LerArquivo",
+    "std::vector<std::string>",
+    "std::string",
+    "dic",
+    "listDir",
+    "diretorio",
+    "execMacroClick",
+    "resetClicks",
+    "click",
+    "pressKeys",
+    "std::vector<WORD>",
+    "keyCodes",
+    "typeText",
+    "keyCode",
+    "delay",
+    "onItemPressed",
+    "QListWidgetItem*",
+    "item",
+    "onItemDropped",
+    "updateClikSaves",
+    "startMacro",
+    "initialize"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
+    uint offsetsAndSizes[58];
+    char stringdata0[11];
+    char stringdata1[13];
+    char stringdata2[1];
+    char stringdata3[14];
+    char stringdata4[9];
+    char stringdata5[5];
+    char stringdata6[14];
+    char stringdata7[11];
+    char stringdata8[25];
+    char stringdata9[12];
+    char stringdata10[4];
+    char stringdata11[8];
+    char stringdata12[10];
+    char stringdata13[15];
+    char stringdata14[12];
+    char stringdata15[6];
+    char stringdata16[10];
+    char stringdata17[18];
+    char stringdata18[9];
+    char stringdata19[9];
+    char stringdata20[8];
+    char stringdata21[6];
+    char stringdata22[14];
+    char stringdata23[17];
+    char stringdata24[5];
+    char stringdata25[14];
+    char stringdata26[16];
+    char stringdata27[11];
+    char stringdata28[11];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 12),  // "recordClicks"
+        QT_MOC_LITERAL(24, 0),  // ""
+        QT_MOC_LITERAL(25, 13),  // "recoverClicks"
+        QT_MOC_LITERAL(39, 8),  // "fs::path"
+        QT_MOC_LITERAL(48, 4),  // "path"
+        QT_MOC_LITERAL(53, 13),  // "showMsgRecord"
+        QT_MOC_LITERAL(67, 10),  // "LerArquivo"
+        QT_MOC_LITERAL(78, 24),  // "std::vector<std::string>"
+        QT_MOC_LITERAL(103, 11),  // "std::string"
+        QT_MOC_LITERAL(115, 3),  // "dic"
+        QT_MOC_LITERAL(119, 7),  // "listDir"
+        QT_MOC_LITERAL(127, 9),  // "diretorio"
+        QT_MOC_LITERAL(137, 14),  // "execMacroClick"
+        QT_MOC_LITERAL(152, 11),  // "resetClicks"
+        QT_MOC_LITERAL(164, 5),  // "click"
+        QT_MOC_LITERAL(170, 9),  // "pressKeys"
+        QT_MOC_LITERAL(180, 17),  // "std::vector<WORD>"
+        QT_MOC_LITERAL(198, 8),  // "keyCodes"
+        QT_MOC_LITERAL(207, 8),  // "typeText"
+        QT_MOC_LITERAL(216, 7),  // "keyCode"
+        QT_MOC_LITERAL(224, 5),  // "delay"
+        QT_MOC_LITERAL(230, 13),  // "onItemPressed"
+        QT_MOC_LITERAL(244, 16),  // "QListWidgetItem*"
+        QT_MOC_LITERAL(261, 4),  // "item"
+        QT_MOC_LITERAL(266, 13),  // "onItemDropped"
+        QT_MOC_LITERAL(280, 15),  // "updateClikSaves"
+        QT_MOC_LITERAL(296, 10),  // "startMacro"
+        QT_MOC_LITERAL(307, 10)   // "initialize"
+    },
+    "MainWindow",
+    "recordClicks",
+    "",
+    "recoverClicks",
+    "fs::path",
+    "path",
+    "showMsgRecord",
+    "LerArquivo",
+    "std::vector<std::string>",
+    "std::string",
+    "dic",
+    "listDir",
+    "diretorio",
+    "execMacroClick",
+    "resetClicks",
+    "click",
+    "pressKeys",
+    "std::vector<WORD>",
+    "keyCodes",
+    "typeText",
+    "keyCode",
+    "delay",
+    "onItemPressed",
+    "QListWidgetItem*",
+    "item",
+    "onItemDropped",
+    "updateClikSaves",
+    "startMacro",
+    "initialize"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      16,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  110,    2, 0x0a,    1 /* Public */,
+       3,    1,  111,    2, 0x0a,    2 /* Public */,
+       6,    0,  114,    2, 0x0a,    4 /* Public */,
+       7,    1,  115,    2, 0x0a,    5 /* Public */,
+      11,    1,  118,    2, 0x0a,    7 /* Public */,
+      13,    0,  121,    2, 0x0a,    9 /* Public */,
+      14,    0,  122,    2, 0x0a,   10 /* Public */,
+      15,    0,  123,    2, 0x0a,   11 /* Public */,
+      16,    1,  124,    2, 0x0a,   12 /* Public */,
+      19,    2,  127,    2, 0x0a,   14 /* Public */,
+      19,    1,  132,    2, 0x2a,   17 /* Public | MethodCloned */,
+      22,    1,  135,    2, 0x0a,   19 /* Public */,
+      25,    1,  138,    2, 0x0a,   21 /* Public */,
+      26,    0,  141,    2, 0x0a,   23 /* Public */,
+      27,    0,  142,    2, 0x0a,   24 /* Public */,
+      28,    0,  143,    2, 0x0a,   25 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void,
+    0x80000000 | 8, 0x80000000 | 9,   10,
+    0x80000000 | 8, 0x80000000 | 4,   12,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 17,   18,
+    QMetaType::Void, QMetaType::Long, QMetaType::Int,   20,   21,
+    QMetaType::Void, QMetaType::Long,   20,
+    QMetaType::Void, 0x80000000 | 23,   24,
+    QMetaType::Void, 0x80000000 | 23,   24,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'recordClicks'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'recoverClicks'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<fs::path, std::false_type>,
+        // method 'showMsgRecord'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'LerArquivo'
+        QtPrivate::TypeAndForceComplete<std::vector<std::string>, std::false_type>,
+        QtPrivate::TypeAndForceComplete<std::string, std::false_type>,
+        // method 'listDir'
+        QtPrivate::TypeAndForceComplete<std::vector<std::string>, std::false_type>,
+        QtPrivate::TypeAndForceComplete<fs::path, std::false_type>,
+        // method 'execMacroClick'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'resetClicks'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'click'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pressKeys'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const std::vector<WORD> &, std::false_type>,
+        // method 'typeText'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const long, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'typeText'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const long, std::false_type>,
+        // method 'onItemPressed'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>,
+        // method 'onItemDropped'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>,
+        // method 'updateClikSaves'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'startMacro'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'initialize'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->recordClicks(); break;
+        case 1: _t->recoverClicks((*reinterpret_cast< std::add_pointer_t<fs::path>>(_a[1]))); break;
+        case 2: _t->showMsgRecord(); break;
+        case 3: { std::vector<std::string> _r = _t->LerArquivo((*reinterpret_cast< std::add_pointer_t<std::string>>(_a[1])));
+            if (_a[0]) *reinterpret_cast< std::vector<std::string>*>(_a[0]) = std::move(_r); }  break;
+        case 4: { std::vector<std::string> _r = _t->listDir((*reinterpret_cast< std::add_pointer_t<fs::path>>(_a[1])));
+            if (_a[0]) *reinterpret_cast< std::vector<std::string>*>(_a[0]) = std::move(_r); }  break;
+        case 5: _t->execMacroClick(); break;
+        case 6: _t->resetClicks(); break;
+        case 7: _t->click(); break;
+        case 8: _t->pressKeys((*reinterpret_cast< std::add_pointer_t<std::vector<WORD>>>(_a[1]))); break;
+        case 9: _t->typeText((*reinterpret_cast< std::add_pointer_t<long>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[2]))); break;
+        case 10: _t->typeText((*reinterpret_cast< std::add_pointer_t<long>>(_a[1]))); break;
+        case 11: _t->onItemPressed((*reinterpret_cast< std::add_pointer_t<QListWidgetItem*>>(_a[1]))); break;
+        case 12: _t->onItemDropped((*reinterpret_cast< std::add_pointer_t<QListWidgetItem*>>(_a[1]))); break;
+        case 13: _t->updateClikSaves(); break;
+        case 14: _t->startMacro(); break;
+        case 15: _t->initialize(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 16)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 16;
+    }
+    return _id;
+}
+QT_WARNING_POP
